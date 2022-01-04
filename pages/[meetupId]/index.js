731ca2bc120391fa3f11b99d152bb13c1700f604
Ivan <ipyka>
2022-01-04 13:27:57 +0200

    added detail page

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail"
+
+const MeetupsDetailsPage = () => {
+  return <MeetupDetail
+    image='https://i2.wp.com/girleatworld.net/wp-content/uploads/2017/12/6108ee0e-1bab-4162-98d3-5665437ae13d.jpg?fit=1400%2C1050&ssl=1'
+    title='First Meetup'
+    address='Some street 5'
+    description='Some Description'
+  />
+}
+
+export default MeetupsDetailsPage;
